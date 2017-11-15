b82ecf1182f0870ad2f94c212f2b02e4af07bdad
Phil Switlick <switlpc1@Phils-MacBook-Pro.local>
2017-11-14 20:26:06 -0500

    Push before presentation

--- scripts/parksFilter.js
+++ scripts/parksFilter.js
@@ -3,13 +3,17 @@ angular
 	.filter('parksFilter', function() {
 		return function(listings, filterChosen) {
 			var filtered = [];
+
 			if (typeof filterChosen == 'undefined')
 				return listings
 			angular.forEach(listings, function(listing) {
+				if (filterChosen == "all")
+					filtered.push(listing);
 				if (listing.details.pets_allowed == "true" && filterChosen == "pets_allowed")
 					filtered.push(listing);
-				else if (listing.details.alcohol_allowed == 'true' && filterChosen == "alcohol_allowed")
+				if (listing.details.alcohol_allowed == 'true' && filterChosen == "alcohol_allowed")
 					filtered.push(listing);
+
 			});
 			return filtered
 		}
